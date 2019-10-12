a24a9f270deb1edf3183866e3d98f51231908d44
Bastien Boutonnet <bastien.b1@gmail.com>
2019-10-12 16:39:17 +0200

    create setup file

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+NAME = "sheetload"
+VERSION = "0.0.0"
+DESCRIPTION = """
+                sheetload is a command line tool to load sheets from google
+                and upload them to snowflake
+                """
+REQUIRED = [
+    # "git+ssh://git@github.com/tripactions/Data_Tooling.git@v1.8.7#egg=data-tools", # TODO:not sure how we can make that work
+    "pandas",
+    "pyyaml",
+    "snowflake==0.0.3",
+    "snowflake-ingest==0.9.1",
+    "snowflake-connector-python==1.7.4",
+    "tqdm",
+]
+REQUIRES_PYTHON = ">=3.6.0"
+
+
+setup(
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=DESCRIPTION,
+    author="Bastien Boutonnet",
+    author_email="bastien.b1@gmail.com",
+    install_requires=REQUIRED,
+    python_requires=REQUIRES_PYTHON,
+    entry_points={"console_scripts": ["sheetload=sheetload.sheetload:run"]},
+)
