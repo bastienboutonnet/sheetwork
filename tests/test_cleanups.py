8766466daae7d2898e9ccec210b5b4dc94584a3f
Bastien Boutonnet <bastien.b1@gmail.com>
2019-11-16 16:22:09 +0100

    Implement test_cleanup

---
+++ tests/test_cleanups.py
@@ -0,0 +1,27 @@
+import pandas
+
+DIRTY_DF = {
+    "col_a": [1, 2, 32],
+    "col b": ["as .    ", "b", "   c"],
+    "1. col_one": ["aa", "bb", "cc"],
+}
+CLEAN_DF = {
+    "col_a": {0: 1, 1: 2, 2: 32},
+    "col b": {0: "as .", 1: "b", 2: "c"},
+    " col_one": {0: "aa", 1: "bb", 2: "cc"},
+}
+
+
+def generate_test_df(df):
+    test_df = pandas.DataFrame.from_dict(df)
+    return test_df
+
+
+def test_cleanup():
+    from sheetload.sheetload import SheetBag
+
+    dirty_df = generate_test_df(DIRTY_DF)
+    clean_df = SheetBag.cleanup(dirty_df)
+    expected_df = generate_test_df(CLEAN_DF)
+
+    assert clean_df.equals(expected_df)
