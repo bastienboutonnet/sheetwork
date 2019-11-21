b40840a49c3e33ddbd8867f59a5ba44bf55eeac5
Bastien Boutonnet <bastien.b1@gmail.com>
2019-11-21 19:42:34 +0100

    Fix broken tests and add extra coverage for column drop

--- tests/test_cleanups.py
+++ tests/test_cleanups.py
@@ -4,11 +4,12 @@ DIRTY_DF = {
     "col_a": [1, 2, 32],
     "col b": ["as .    ", "b", "   c"],
     "1. col_one": ["aa", "bb", "cc"],
+    "": ["q", "q", "q"],
 }
 CLEAN_DF = {
     "col_a": {0: 1, 1: 2, 2: 32},
-    "col b": {0: "as .", 1: "b", 2: "c"},
-    " col_one": {0: "aa", 1: "bb", 2: "cc"},
+    "col_b": {0: "as .", 1: "b", 2: "c"},
+    "col_one": {0: "aa", 1: "bb", 2: "cc"},
 }
 
 
@@ -18,10 +19,10 @@ def generate_test_df(df):
 
 
 def test_cleanup():
-    from sheetload.sheetload import SheetBag
+    from sheetload.cleaner import SheetCleaner
 
     dirty_df = generate_test_df(DIRTY_DF)
-    clean_df = SheetBag.cleanup(dirty_df)
+    clean_df = SheetCleaner(dirty_df).cleanup()
     expected_df = generate_test_df(CLEAN_DF)
 
     assert clean_df.equals(expected_df)
