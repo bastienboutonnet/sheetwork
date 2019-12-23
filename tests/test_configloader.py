3a999063cd49773f3bf6f5648fada48196b2b4e7
Bastien Boutonnet <bastien.b1@gmail.com>
2019-12-23 22:08:13 +0100

    Fix pointless statement

--- tests/test_configloader.py
+++ tests/test_configloader.py
@@ -5,6 +5,5 @@ def test_set_config():
     from sheetload.config import ConfigLoader
 
     config = ConfigLoader(test=True)
-    config.sheet_config
 
-    assert ConfigLoader(test=True).sheet_config == EXPECTED_CONFIG
+    assert config.sheet_config == EXPECTED_CONFIG
