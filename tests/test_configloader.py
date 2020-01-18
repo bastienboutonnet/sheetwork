96573931eae45ee02ed40225f5f96272f5155db2
Bastien Boutonnet <bastien.b1@gmail.com>
2020-01-18 13:39:56 +0100

    Add tests for case when no cols are in config

--- tests/test_configloader.py
+++ tests/test_configloader.py
@@ -4,7 +4,7 @@ import pytest
 
 from sheetload.flags import FlagParser
 
-from .mockers import EXPECTED_CONFIG
+from .mockers import EXPECTED_CONFIG, NO_COLS_EXPECTED_CONFIG
 
 FIXTURE_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)))
 
@@ -17,3 +17,13 @@ def test_set_config(datafiles):
     config = ConfigLoader(flags, yml_folder=str(datafiles))
 
     assert config.sheet_config == EXPECTED_CONFIG
+
+
+@pytest.mark.datafiles(FIXTURE_DIR)
+def test_set_config(datafiles):
+    from sheetload.config import ConfigLoader
+
+    flags = FlagParser(test_sheet_name="no_cols")
+    config = ConfigLoader(flags, yml_folder=str(datafiles))
+
+    assert config.sheet_config == NO_COLS_EXPECTED_CONFIG
