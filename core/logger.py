f2b7ef76975854b7a8945560e85eba162bfb112d
Bastien Boutonnet <bastien.b1@gmail.com>
2020-01-20 19:45:57 +0100

    Implement logger with output to file

--- core/logger.py
+++ core/logger.py
@@ -1,16 +1,36 @@
 import logging
 
-from data_tools.logging import LoggerFactory
-
 
 class LogManager:
-    def __init__(self, logger: LoggerFactory):
-        self.logger = logger.get_logger(level=getattr(logging, "info".upper()))
+    def __init__(self):
+        logger = logging.getLogger("Sheetload Logger")
+        logger.setLevel(logging.INFO)
+        # Create handlers
+        c_handler = logging.StreamHandler()
+        f_handler = logging.FileHandler("file.log")
+        c_handler.setLevel(logging.INFO)
+        f_handler.setLevel(logging.INFO)
+
+        # Create formatters and add it to handlers
+        c_format = logging.Formatter("%(name)s - %(levelname)s - %(funcName)s - %(message)s")
+        f_format = logging.Formatter(
+            "%(asctime)s - %(name)s - %(levelname)s - %(funcName)s - %(message)s"
+        )
+        c_handler.setFormatter(c_format)
+        f_handler.setFormatter(f_format)
+
+        # Add handlers to the logger
+        logger.addHandler(c_handler)
+        logger.addHandler(f_handler)
+
+        self.logger = logger
 
     def set_debug(self):
-        self.logger = self.logger.setLevel(logging.DEBUG)
+        self.logger.setLevel(logging.DEBUG)
+        for handler in self.logger.handlers:
+            handler.setLevel(logging.DEBUG)
 
 
-log_manager = LogManager(LoggerFactory)
+log_manager = LogManager()
 
 GLOBAL_LOGGER = log_manager.logger
