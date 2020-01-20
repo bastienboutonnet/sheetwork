56f0ede01912bc921a0a83e141bb1d7c349e465f
Bastien Boutonnet <bastien.b1@gmail.com>
2020-01-20 13:19:36 +0100

    Bump version: 0.2.1b0 → 0.2.1b1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 NAME = "sheetload"
-VERSION = "0.2.1b0"
+VERSION = "0.2.1b1"
 DESCRIPTION = """
                 sheetload is a command line tool to load sheets from google
                 and upload them to snowflake
