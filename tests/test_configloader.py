54711ad4e4d843560d24bb9dd621da2e19fb81ce
Bastien Boutonnet <bastien.b1@gmail.com>
2020-04-12 14:53:12 +0200

    Add test on override_cli_args

--- tests/test_configloader.py
+++ tests/test_configloader.py
@@ -36,3 +36,21 @@ def test_set_config(datafiles):
 
     assert config.sheet_config == EXPECTED_CONFIG
     assert config2.sheet_config == NO_COLS_EXPECTED_CONFIG
+
+
+@pytest.mark.datafiles(FIXTURE_DIR)
+def test__override_cli_args(datafiles):
+    from core.config.config import ConfigLoader
+
+    from core.config.project import Project
+    from core.main import parser
+
+    flags = FlagParser(
+        parser,
+        test_sheet_name="sheet_with_no_schema",
+        project_dir=str(datafiles),
+        sheet_config_dir=str(datafiles),
+    )
+    project = Project(flags)
+    config = ConfigLoader(flags, project)
+    assert config.target_schema == project.target_schema
