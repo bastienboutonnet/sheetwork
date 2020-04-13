7225139674766bd1d8c26d603fefac5f01a25535
Bastien Boutonnet <bastien.b1@gmail.com>
2020-04-13 14:51:43 +0200

    Add some spice to setup

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import os
 from setuptools import find_packages, setup
 
 NAME = "sheetload"
@@ -11,12 +12,16 @@ REQUIRED = [line.strip() for line in open("pip-requirements.txt", "r")]
 
 REQUIRES_PYTHON = ">=3.6.0"
 
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, "README.md")) as f:
+    long_description = f.read()
 
 setup(
     name=NAME,
     version=VERSION,
     description=DESCRIPTION,
-    long_description=DESCRIPTION,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Bastien Boutonnet",
     author_email="bastien.b1@gmail.com",
     install_requires=REQUIRED,
