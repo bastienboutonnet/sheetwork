b779d832672505f1dda613e5c77cb091913c4b7f
Bastien Boutonnet <bastien.b1@gmail.com>
2020-12-21 09:06:53 +0100

    add more tests on flags

--- tests/flags_test.py
+++ tests/flags_test.py
@@ -3,7 +3,6 @@ def test_consume_cli_arguments():
     from sheetwork.core.main import parser
 
     flag_parser = FlagParser(parser)
-    print("AKJKSDJHSKDJHS")
     flag_parser.consume_cli_arguments(["init", "--project-name", "sheetwork_test"])
     assert flag_parser.task == "init"
     assert flag_parser.project_name == "sheetwork_test"
