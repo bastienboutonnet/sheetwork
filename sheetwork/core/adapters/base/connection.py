93fce514eb5b30a5f3cce85cf11fdab945273739
Bastien Boutonnet <bastien.b1@gmail.com>
2020-12-30 22:19:31 +0100

    refactor base connection

--- sheetwork/core/adapters/base/connection.py
+++ sheetwork/core/adapters/base/connection.py
@@ -6,12 +6,8 @@ class BaseCredentials(abc.ABC):
     """Base class to define basic API contract for a Credentials class and its methods."""
 
     @abc.abstractmethod
-    def validate_credentials(self) -> None:
-        raise NotImplementedError()
-
-    @abc.abstractmethod
-    def parse_credentials(self) -> None:
-        raise NotImplementedError()
+    def parse_and_validate_credentials(self):
+        ...
 
 
 class BaseConnection(abc.ABC):  # noqa D101
