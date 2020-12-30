a003cd4c67e709a1462b6b9269a089d405674329
Bastien Boutonnet <bastien.b1@gmail.com>
2020-12-30 18:59:02 +0100

    refactor: use pydantic for snowflake credentials validation

--- sheetwork/core/adapters/base/connection.py
+++ sheetwork/core/adapters/base/connection.py
@@ -18,3 +18,17 @@ class BaseConnection(abc.ABC):  # noqa D101
     @abc.abstractmethod
     def generate_engine(self) -> None:
         raise NotImplementedError()
+
+
+def check_db_type_compatibility(field: str, expected_db: str) -> str:
+    """Pydantic validation decocator that checks that the a field has an expected value.
+
+    Args:
+        field (str): Value to check.
+        expected_db (str): Name of the expected database.
+
+    Returns:
+        str: the value of the field to check if validation is passed.
+    """
+    assert field == expected_db
+    return field
